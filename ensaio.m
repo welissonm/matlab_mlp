d33429e4b43b08a11467e04fde6946a6145d55b6
Welisson <welissonm@outlook.com>
2018-11-29 18:07:24 -0300

    modificando a função ensaio

--- ensaio.m
+++ ensaio.m
@@ -1,4 +1,11 @@
 function [t,y] = ensaio(tspan, timeSample, amp, delay)
-    [t, prs] = prs_t(amp, delay,timeSample,tspan);
-    
+    [t, prs] = prs_t(amp,delay,timeSample,tspan);
+    m = mean(prs);
+    y = prs - m;
+    mini = min(y);
+    maxi = max(y);
+    if(mini < 0)
+        delta = amp(2) - (abs(mini) + maxi);
+        y = y + delta;
+    end
 end
