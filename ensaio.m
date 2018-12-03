354313f8c94adcd8e975ed6ad92eea9f5a7ea08f
Welisson Moura <welissonsantos@tjrn.jus.br>
2018-12-03 12:06:52 -0300

    conclusão da implementação da função para realizar o ensaio na planta. Entretanto ainda não foi validado

--- ensaio.m
+++ ensaio.m
@@ -1,6 +1,5 @@
 function [t,y] = ensaio(tspan, timeSample, amp, delay)
     [t, prs, t_trans, u_trans] = prs_t(amp,delay,timeSample,tspan);
-    t_trans
     m = mean(prs);
     mini = min(prs);
     maxi = max(prs);
@@ -13,7 +12,7 @@ function [t,y] = ensaio(tspan, timeSample, amp, delay)
     plot(t,prs);
     u_trans
     G = tf([0.0194],[1 0.1312 0.004302]);
-    Gd = c2d(G,0.001);
+    Gd = c2d(G,timeSample);
     [Ad, Bd, Cd, Dd] = tf2ss(Gd.num, Gd.den);
     y = zeros(1,length(t));
     x = y;
@@ -22,17 +21,30 @@ function [t,y] = ensaio(tspan, timeSample, amp, delay)
     sum_y = 0;
     my = 0;
     ii = 1;
+	h = 0;
     for i=2:1length(t)
       if(t(i) == t_trans(j))
         my = 0;
         j = j+1;
         ii = i;
+		h = 2 + eps;
+		k = 1;
       else
         k = k +1;
         sum_y = sum_y + y(i-1); %media do sinal;
         my = sum_y/k
         sy = sqrt((1/k)*sum(y(ii: i-1) - my));% desvio padrao da saida
+		if(my > 0)
+			h = (sy/my)*100.0;
+		else
+			warning('saida media do sistema eh zero');
+		end	
       end
+	  if(h <= 2 && k >2)
+		u(i-1) = randNum(-1.02*prs(i-1),1.02*prs(i-1));
+	  else
+		u(i-1) = prs(i-1);
+	  end
       x(i) = Ad*x(i-1) + Bd*u(i-1);
       y(i) = C*x(i-1) + Dd*u(i-1);
     end
