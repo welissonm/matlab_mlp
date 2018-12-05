db8b74bd639fe8bb35a8e97f0145bf2cf10c821b
Welisson Moura <welissonsantos@tjrn.jus.br>
2018-12-05 13:01:32 -0300

    correções no algoritmo de Levenberg-Marquardt

--- levenbergM.m
+++ levenbergM.m
@@ -60,8 +60,7 @@ function newff = levenbergM(dataset,nnet, tol,varargin)
 		epoch = epoch+1;
 		for k=1:p:m-p
 			kp = k+p-1;
-      
-      J = [];
+      J = cell(1,nnet.layers);
 			for j=k:kp
         dW = cell(1,nnet.layers);
 				[y,newff] = sim(dataset.data(:,j),newff);
@@ -71,28 +70,22 @@ function newff = levenbergM(dataset,nnet, tol,varargin)
 				sumErrorQ = sumErrorQ + erro.^2;
 				%V = sum(sum(err2,1),2);
 				delta{1,newff.layers} = erro.*dlogsim(newff.layer{1,newff.layers}.net);
-          for i=newff.layers-1:-1:1
+        for i=newff.layers-1:-1:1
 					dW{1,i+1} = [dW{1,i+1};kron(delta{1,i+1},newff.layer{1,i}.y')];
-					J = [J, dW{1,i+1}];
-					%wNew{1,i+1} = newff.layer{1,i+1}.w + alpha*(newff.layer{1,i+1}.w - wOld{1,i+1}) + eta*dW{1,i+1};
-					%wOld{1,i+1} = newff.layer{1,i+1}.w;
-					%newff.layer{1,i+1}.w = wNew{1,i+1};
+					J{1,i+1} = [J{1,i+1}; reshape(dW{1,i+1}',1,prod(size(newff.layer{1,i+1}.w)))];
 					delta{1,i} = -(newff.layer{1,i+1}.w'*delta{1,i+1}).*dlogsim(newff.layer{1,i}.net);
 				end
 				dW{1,1} = [dW{1,1};kron(delta{1,1},dataset.data(:,1)')];
-				J = [J, dW{1,1}];
+				J{1,1} = [J{1,1}; reshape(dW{1,1}',1,prod(size(newff.layer{1,1}.w)))];
         clear dW;
-				%wNew{1,1} = newff.layer{1,1}.w + alpha*(newff.layer{1,1}.w - wOld{1,1}) + eta*dW{1,1};
-				%wOld{1,1} = newff.layer{1,1}.w;
-				%newff.layer{1,1}.w = wNew{1,1};
 			end
-			J2 = J'*J;
-			dW = ((J2 + eta*eye(size(J2)))^-1 )*J'*E;
-      clear J;
-			for i=1:newff.layers
-         wNew{1,i} = newff.layer{1,i}.w + reshape(dW(1:prod(size(newff.layer{1,i}.w)),i),size(newff.layer{1,i}.w))';
-         newff.layer{1,i}.w = wNew{1,i};
+      for i=1:newff.layers
+        J2 = J{1,i}'*J{1,i};
+			  dW = ((J2 + eta*eye(size(J2)))^-1 )*J{1,i}'*E;
+        wNew{1,i} = newff.layer{1,i}.w + reshape(dW(1:prod(size(newff.layer{1,i}.w)),i),size(newff.layer{1,i}.w))';
+        newff.layer{1,i}.w = wNew{1,i};
       end
+      clear J;
 		end
 		eqm = sumErrorQ/m;
 		if(sqrt(max(sumErrorQ)/m) <= tol)
